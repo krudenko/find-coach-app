16abb1fb495db09a02c9bbfaa720511a51294e0e
Kseniia Rudenko <ksenia1711@gmail.com>
2025-07-04 16:30:35 +0200

    Added requests to Firebase, Spinner, Error Handling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,14 +1,20 @@
 <template>
+    <base-dialog :show="!!error" title="An error occured!" @close="handleError" >
+        <p>{{ error }}</p>
+    </base-dialog>
     <section>
         <coach-filter @change-filter="setFilters"></coach-filter>
     </section>
     <section>
         <base-card>
             <div class="controls">
-                <base-button mode="outline">Refresh</base-button>
-                <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
+                <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
+                <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
             </div>
-            <ul v-if="hasCoaches">
+            <div v-if="isLoading">
+                <base-spinner></base-spinner>
+            </div>
+            <ul v-else-if="hasCoaches">
                 <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id"
                     :first-name="coach.firstName" :last-name="coach.lastName" :rate="coach.hourlyRate"
                     :areas="coach.areas">
@@ -30,11 +36,13 @@ export default {
     },
     data() {
         return {
+            isLoading: false,
+            error: null,
             activeFilters: {
                 frontend: true,
                 backend: true,
                 career: true
-            }
+            },
         }
     },
     computed: {
@@ -57,13 +65,28 @@ export default {
             });
         },
         hasCoaches() {
-            return this.$store.getters['coaches/hasCoaches'];
+            return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
         }
     },
     methods: {
         setFilters(updatedFilters) {
             this.activeFilters = updatedFilters;
+        },
+        async loadCoaches() {
+            this.isLoading = true;
+            try {
+                await this.$store.dispatch('coaches/loadCoaches');
+                this.isLoading = false;
+            } catch (error) {
+                this.error = error.message || 'Something went wrong!';
+            }
+        },
+        handleError() {
+            this.error = null;
         }
+    },
+    created() {
+        this.loadCoaches();
     }
 }
 </script>
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,58 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId, // getters of store/index.js
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit('registerCoach', coachData);
+
+    // Add coach to Firebase
+    const response = await fetch(
+      `https://vue-find-coach-cea44-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    if (!response.ok) {
+      // error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
+  async loadCoaches(context) {
+    // Get coaches from Firebase
+    const response = await fetch(
+      `https://vue-find-coach-cea44-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   },
 };
